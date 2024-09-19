e01ed13d5435e3d10f54cdba3abaf5e5144f71ed
eltsovya <eltsovya@310D-10>
2024-09-19 12:00:34 +0700

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _19._02._24_1_ZAD
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _19._02._24_1_ZAD
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangeColor_Click(object sender, RoutedEventArgs e)
+        {
+            if (Maingrid.Background == Brushes.LightGray)
+            {
+                Maingrid.Background = Brushes.LightBlue;
+            }
+            else
+            {
+                Maingrid.Background = Brushes.LightGray;
+            }
+        }
+
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("блабалбалбалаблаблаблабл", "About");
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        
+    }
+}
